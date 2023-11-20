d9906ae51cbed347b2b4ccfee16188d086b3c73d
Plinioc <plinio.lsc@gmail.com>
2023-11-20 16:16:40 -0300

    Initial map tool structure

--- public/map-tools/brazil-municipalities-tools.js
+++ public/map-tools/brazil-municipalities-tools.js
@@ -1,10 +1,21 @@
 const statesSvgPath = './map-tools/assets/brazil-states.svg'
 const municipalitiesSvgPath = './map-tools/assets/municipalities-map.svg'
 
-async function renderMap(selector) {
-  // const img = document.createElement('img')
-  // img.src = municipalitiesSvgPath
-  const path = await fetch(municipalitiesSvgPath).then(res => res.text())
-  console.log('path', path)
-  document.querySelector(selector).innerHTML = path
+class MunicipalitiesMapBuilder {
+  constructor() {
+    this.rendered = false
+    this.selector = ''
+  }
+
+  async renderMap(selector) {
+    if(this.rendered) {
+      console.error('Render map error: maps can only be rendered once')
+      return
+    }
+
+    const path = await fetch(municipalitiesSvgPath).then(res => res.text())
+    document.querySelector(selector).innerHTML = path
+
+    this.rendered = true
+  }
 }
