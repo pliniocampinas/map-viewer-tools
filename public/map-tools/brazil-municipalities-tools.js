b5b9488d9c81bbd29bd26da502ec0989d08d9656
Plinioc <plinio.lsc@gmail.com>
2023-11-20 19:18:02 -0300

    Configure map builder with constructor parameters

--- public/map-tools/brazil-municipalities-tools.js
+++ public/map-tools/brazil-municipalities-tools.js
@@ -2,27 +2,29 @@ const statesSvgPath = './map-tools/assets/brazil-states.svg'
 const municipalitiesSvgPath = './map-tools/assets/municipalities-map.svg'
 
 class MunicipalitiesMapBuilder {
-  constructor() {
+  constructor({containerSelector, onPathClick, selectedPathClass}) {
     this.rendered = false
-    this.selector = ''
-    this.onPathClick = ({code, description}) => {
+    this.containerSelector = containerSelector
+    this.selectedPathClass = selectedPathClass
+    this.onPathClick = onPathClick || (({code, description}) => {
       console.log('click code:', code, 'description', description)
-    }
+    })
+    this.pathElementsMap = {}
+    this.selectedCode = ''
   }
 
-  async renderMap(selector) {
+  async render() {
     if(this.rendered) {
       console.error('Render map error: maps can only be rendered once')
       return
     }
 
     const path = await fetch(municipalitiesSvgPath).then(res => res.text())
-    const containerElement = document.querySelector(selector)
+    const containerElement = document.querySelector(this.containerSelector)
     if(!containerElement) {
       console.error('Render map error: container element not found')
       return
     }
-    this.selector = selector
     containerElement.innerHTML = path
 
     for (const pathElement of containerElement.querySelectorAll('path')) {
@@ -35,13 +37,33 @@ class MunicipalitiesMapBuilder {
         code,
         description
       }))
+      this.pathElementsMap[code] = pathElement
     }
 
     // Finally
     this.rendered = true
+    return this
+  }
+
+  toogleSelectPath(code) {
+    if(this.selectedCode) {
+      const prevElement = this.pathElementsMap[this.selectedCode]
+      prevElement.classList.remove(this.selectedPathClass)
+    }
+
+    if(this.selectedCode == code) {
+      this.selectedCode = ''
+      return
+    }
+
+    const element = this.pathElementsMap[code]
+    if(!element) {
+      console.warn('Path not found for code', code)
+    }
+    if(!this.selectedPathClass) {
+      console.warn('There is no selectedPathClass configured')
+    }
+    element.classList.add(this.selectedPathClass)
+    this.selectedCode = code
   }
 }
-
-// const getPathElement = (code: string) => {
-//   return chartContainerElement.value?.querySelector(`path[citycode="${code}"]`)
-// }
