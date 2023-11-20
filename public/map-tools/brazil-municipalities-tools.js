37f5c002570c69775bb4be06af63baabc4adec86
Plinioc <plinio.lsc@gmail.com>
2023-11-20 16:55:52 -0300

    onPath click

--- public/map-tools/brazil-municipalities-tools.js
+++ public/map-tools/brazil-municipalities-tools.js
@@ -5,6 +5,9 @@ class MunicipalitiesMapBuilder {
   constructor() {
     this.rendered = false
     this.selector = ''
+    this.onPathClick = ({code, description}) => {
+      console.log('click code:', code, 'description', description)
+    }
   }
 
   async renderMap(selector) {
@@ -14,8 +17,31 @@ class MunicipalitiesMapBuilder {
     }
 
     const path = await fetch(municipalitiesSvgPath).then(res => res.text())
-    document.querySelector(selector).innerHTML = path
+    const containerElement = document.querySelector(selector)
+    if(!containerElement) {
+      console.error('Render map error: container element not found')
+      return
+    }
+    this.selector = selector
+    containerElement.innerHTML = path
 
+    for (const pathElement of containerElement.querySelectorAll('path')) {
+      const code = pathElement.getAttribute('citycode')
+      const description = pathElement.getAttribute('description')
+      if(!code) {
+        continue
+      }
+      pathElement.addEventListener('click', () => this.onPathClick({
+        code,
+        description
+      }))
+    }
+
+    // Finally
     this.rendered = true
   }
 }
+
+// const getPathElement = (code: string) => {
+//   return chartContainerElement.value?.querySelector(`path[citycode="${code}"]`)
+// }
